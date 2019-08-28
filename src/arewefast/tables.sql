90bb3b93645781ebddbddaf4cb3d94467d8d110c
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2019-08-28 19:43:54 +0200

    Update table structure.

--- src/arewefast/tables.sql
+++ src/arewefast/tables.sql
@@ -1,22 +1,22 @@
  CREATE TABLE `benchmark` (
   `name` varchar(255) NOT NULL,
-  `time` datetime NOT NULL,
+  `time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `duration` int(11) DEFAULT NULL
 );
 
  CREATE TABLE `build` (
   `task` varchar(255) NOT NULL,
-  `time` datetime NOT NULL,
+  `time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `duration` int(11) DEFAULT NULL
 );
 
 CREATE TABLE `phase` (
   `phase` varchar(255) NOT NULL,
-  `time` datetime NOT NULL,
+  `time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `duration` bigint(20) NOT NULL
 );
 
  CREATE TABLE `throughput` (
-  `time` datetime NOT NULL,
+  `time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `rate` bigint(20) NOT NULL
 );
