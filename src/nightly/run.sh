afd7e0b1b1a72738a24642e1a182040e5c77f54c
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2020-05-14 13:12:55 +0200

    Added nightly build script.

---
+++ src/nightly/run.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/fish
+
+cd /home/flix/nightly
+
+rm -rf /home/flix/nightly/flix
+
+git clone https://github.com/flix/flix.git
+
+cd /home/flix/nightly/flix
+
+./gradlew jar
+
+cp build/libs/flix.jar /srv/www/flix.dev/wwwroot/nightly/flix-(date -I).jar
