185e4aea16d4233fae3c2441a7303759467348af
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2021-01-29 09:57:53 +0100

    Fix some nullity.

--- src/arewefast/tables.sql
+++ src/arewefast/tables.sql
@@ -3,16 +3,15 @@ CREATE TABLE `benchmark_ext`
     `time`     datetime     NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `threads`  bigint(20)   NOT NULL,
     `name`     varchar(255) NOT NULL,
-    `duration` int(11)               DEFAULT NULL
+    `duration` int(11)      NOT NULL
 );
 
 CREATE TABLE `build`
 (
     `task`     varchar(255) NOT NULL,
     `time`     datetime     NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `duration` int(11)               DEFAULT NULL
+    `duration` int(11)      NOT NULL
 );
-
 
 CREATE TABLE `phase_ext`
 (
@@ -28,7 +27,7 @@ CREATE TABLE `playground`
 (
     `time`   datetime     NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `status` varchar(255) NOT NULL,
-    `count`  int(11)               DEFAULT NULL
+    `count`  int(11)      NOT NULL
 );
 
 CREATE TABLE `throughput_ext`
