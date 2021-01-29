bcce2820043c233d5ec8e5cc7115b717bf0218eb
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2021-01-29 09:56:36 +0100

    Reformat.

--- src/arewefast/tables.sql
+++ src/arewefast/tables.sql
@@ -1,38 +1,44 @@
-CREATE TABLE `build` (
-  `task` varchar(255) NOT NULL,
-  `time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `duration` int(11) DEFAULT NULL
+CREATE TABLE `benchmark_ext`
+(
+    `time`     datetime     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `threads`  bigint(20)   NOT NULL,
+    `name`     varchar(255) NOT NULL,
+    `duration` int(11)               DEFAULT NULL
 );
 
-CREATE TABLE `playground` (
-   `time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-   `status` varchar(255) NOT NULL,
-   `count` int(11) DEFAULT NULL
- );
-
-CREATE TABLE `benchmark_ext` (
-    `time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `threads` bigint(20) NOT NULL,
-    `name` varchar(255) NOT NULL,
-    `duration` int(11) DEFAULT NULL
+CREATE TABLE `build`
+(
+    `task`     varchar(255) NOT NULL,
+    `time`     datetime     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `duration` int(11)               DEFAULT NULL
 );
 
-CREATE TABLE `phase_ext` (
-    `time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `phase` varchar(255) NOT NULL,
-    `lines` bigint(20) NOT NULL,
-    `threads` bigint(20) NOT NULL,
-    `iterations` bigint(20) NOT NULL,
-    `duration` bigint(20) NOT NULL
+
+CREATE TABLE `phase_ext`
+(
+    `time`       datetime     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `phase`      varchar(255) NOT NULL,
+    `lines`      bigint(20)   NOT NULL,
+    `threads`    bigint(20)   NOT NULL,
+    `iterations` bigint(20)   NOT NULL,
+    `duration`   bigint(20)   NOT NULL
 );
 
-CREATE TABLE `throughput_ext` (
-    `time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `lines` bigint(20) NOT NULL,
-    `threads` bigint(20) NOT NULL,
-    `iterations` bigint(20) NOT NULL,
-    `minThroughput` bigint(20) NOT NULL,
-    `maxThroughput` bigint(20) NOT NULL,
-    `avgThroughput` bigint(20) NOT NULL,
+CREATE TABLE `playground`
+(
+    `time`   datetime     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `status` varchar(255) NOT NULL,
+    `count`  int(11)               DEFAULT NULL
+);
+
+CREATE TABLE `throughput_ext`
+(
+    `time`             datetime   NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `lines`            bigint(20) NOT NULL,
+    `threads`          bigint(20) NOT NULL,
+    `iterations`       bigint(20) NOT NULL,
+    `minThroughput`    bigint(20) NOT NULL,
+    `maxThroughput`    bigint(20) NOT NULL,
+    `avgThroughput`    bigint(20) NOT NULL,
     `medianThroughput` bigint(20) NOT NULL
 );
